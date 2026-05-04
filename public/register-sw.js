35092f0232fb246c4c4dc84e49a72032953e55d3
alberto40390434 <a42944053@gmail.com>
2026-05-03 20:58:49 -0700

    Refactor service worker registration logic

--- public/register-sw.js
+++ public/register-sw.js
@@ -1,21 +1,21 @@
 "use strict";
 
-const swConfig = {
-    prefix: "/scram/",
-    sw: "/scram/scramjet.sw.js",
-};
+const stockSW = "./sw.js"; // This MUST match the actual file name in your public folder
+const swAllowedHostnames = ["localhost", "127.0.0.1"];
 
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker
-            .register(swConfig.sw, {
-                scope: swConfig.prefix,
-            })
-            .then(() => {
-                console.log("Scramjet Engine Ready");
-            })
-            .catch((err) => {
-                console.error("SW Failed:", err);
-            });
-    });
+async function registerSW() {
+	if (!navigator.serviceWorker) {
+		if (
+			location.protocol !== "https:" &&
+			!swAllowedHostnames.includes(location.hostname)
+		)
+			throw new Error("Service workers cannot be registered without https.");
+
+		throw new Error("Your browser doesn't support service workers.");
+	}
+
+	await navigator.serviceWorker.register(stockSW);
 }
+
+// Automatically try to register on load
+registerSW().catch(console.error);
